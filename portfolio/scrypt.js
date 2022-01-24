1c54b5a333cfaecf06b230cf6c097b44212f7bed
NagaevRoman <nahagaevroman@gmail.com>
2022-01-25 02:52:22 +0300

    feat: create menu burger

--- portfolio/scrypt.js
+++ portfolio/scrypt.js
@@ -1,8 +1,17 @@
 
-// $(document).ready(function(){
-//   $(".hamburger").click(function(){
-//     $(this).toggleClass("is-active");
-//   });
-// });
+let burger = document.querySelector('.burger');
+burger.addEventListener('click', function () {
+    burger.classList.toggle('active');
+})
+let list__header = document.querySelector('.list__header');
+burger.addEventListener('click', function () {
+    list__header.classList.toggle('active');
+})
+const link__header = document.querySelectorAll('.link__header');
+link__header.forEach(n => n.addEventListener('click', closeMenu));
+function closeMenu() {
+    burger.classList.remove('active');
+    list__header.classList.remove('active');
 
-console.log((A && B) || ((!A) &&B));
+
+}
