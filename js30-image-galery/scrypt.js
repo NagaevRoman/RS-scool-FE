9428b6b7b3faf035f58c25e6d11fdeaca144becd
NagaevRoman <78729515+NagaevRoman@users.noreply.github.com>
2022-02-15 12:09:26 +0300

    feat: completed search

--- js30-image-galery/scrypt.js
+++ js30-image-galery/scrypt.js
@@ -2,7 +2,11 @@
 const gallery = document.querySelector('.grid')
 
 const form  = document.getElementsByTagName('form')
-console.log(form);
+// console.log(form);
+
+const textSearch = document.getElementById('inp')
+
+// console.log(textSearch);
 
 const submit = document.querySelector('.searching')
 
@@ -13,44 +17,81 @@ function createImage(url) {
   gallery.appendChild(el)
 }
 
-let searchForm = document.forms[0];
-let elem = searchForm.elements.text;
-console.log(form[0].value);
-// console.log(elem.value);
-// submit.addEventListener('click', function(){
-//   console.log(`${document.form.inp.value}`)
-// })
-function complete(value) {
-  document.onkeydown = null;
-  search = value;
+function removeImg() {
+  while (gallery.firstChild) {
+    gallery.removeChild(gallery.firstChild)
+  }
 }
-form.onsubmit = function() {
-  let value = form[0].value;
-  console.log(value);
-  if (value == '') return false; 
-  return false;
-};
+
+submit.addEventListener("click", () => searchOut(textSearch.value))
 
 
-let search 
-if (search === undefined) {
-  search = 'spring'
+textSearch.addEventListener("keydown", (event) => {
+  const keyName = event.key;
+  if (keyName === 'Enter') {
+    console.log(textSearch.value);
+    searchOut(textSearch.value)
+    // return;
+  }
+})
+
+
+
+let search = 'spring'
+function searchOut(val) {
+  if (val === undefined || val === '') {
+    return search 
+  } else {
+    console.log(search);
+    search = val
+    removeImg()
+    getData();
+    // localStorage.setItem('name', `${val}`);
+    // var myName = localStorage.getItem('name');
+    // console.log(myName);
+  }
 }
+
+
+
 
 
 async function getData() {
   const res = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=0ba8cb4d45c90acd73eeb53fa8ebb494&tags= + ${search} + &orientation=landscape&tag_mode=all&extras=url_m&format=json&nojsoncallback=1`);
   const data = await res.json();
-  // console.log(data);
+
   for (let i = 0; i < data.photos.photo.length; i++) {
     let link = data.photos.photo[i].url_m
-    // let title = data.photos.photo[0].title
+
     createImage(link)
-    // createTitle(title)
+
   }
-  // console.log(data.photos.getInfo.api_key);
+
 }
 getData();
+
+
+// let searchForm = document.forms[0];
+// let elem = searchForm.elements.text;
+// console.log(elem.value);
+
+
+// console.log(elem.value);
+// submit.addEventListener('click', function(){
+//   console.log(`${document.form.inp.value}`)
+// })
+// function complete(value) {
+//   document.onkeydown = null;
+//   search = value;
+// }
+// form.onsubmit = function() {
+//   let value = form[0].value;
+//   console.log(value);
+//   if (value == '') return false; 
+//   return false;
+// };
+
+
 
 
 // function createTitle(title) {
