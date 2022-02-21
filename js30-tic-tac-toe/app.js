8489aac3e124d97c7c3705da2e39c143e1049ed1
NagaevRoman <78729515+NagaevRoman@users.noreply.github.com>
2022-02-22 00:31:27 +0300

    feat: add steps

--- js30-tic-tac-toe/app.js
+++ js30-tic-tac-toe/app.js
@@ -5,6 +5,7 @@ const cont = document.querySelector('.content');
 const modRes = document.querySelector('.modal-result');
 const overlay = document.querySelector('.overlay');
 const close = document.querySelector('.btn-close');
+const steps = document.querySelector('.steps');
 
 const answer = [
   [0,1,2],
@@ -17,6 +18,7 @@ const answer = [
   [2,4,6],
 ]
 
+
 let count = 0;
 let result = '';
 let crossWon = 0;
@@ -24,41 +26,46 @@ let naughtWon = 0;
 
 tablet.addEventListener('click', e => {
   if (e.target.className = 'cell') {
+    count++
     count % 2 !== 0 ?  
     e.target.innerHTML = 'X': 
     e.target.innerHTML = 'O'
-    count++
     checkStatus()
+    if (count === 9) {
+      cont.innerHTML = "Drawn game"
+      modRes.style.display = 'block'
+    }
+    console.log(count);
   }
 })
 
 function checkStatus() {
-  let val = 0
   for (let i = 0; i < answer.length; i++) {
-    
     if (cells[answer[i][0]].innerHTML == 'X' && cells[answer[i][1]].innerHTML == 'X' && cells[answer[i][2]].innerHTML == 'X') {
+       crossWon++
+       console.log(crossWon);
        result = 'cross'
-       val++
-       crossWon += 1
        prResult(result)
-       
     } else if (cells[answer[i][0]].innerHTML == 'O' && cells[answer[i][1]].innerHTML == 'O' && cells[answer[i][2]].innerHTML == 'O') {
+      naughtWon++
        result = 'naught'
-       val++
-       naughtWon += 1
        prResult(result)
     } 
   }
-  
 }
 
+
 const prResult = winner => {
+  steps
+  
   cont.innerHTML = `Congratulations! <br>
   ${winner} won!`
+  steps.innerHTML = `Steps: ${count}`
   modRes.style.display = 'block'
 };
 
 const closeMod = () => {
+  count = 0
   modRes.style.display = 'none'
   for (let i = 0; i < cells.length; i++) {
     cells[i].innerHTML = ''
@@ -73,11 +80,7 @@ close.addEventListener('click', closeMod)
 rest.addEventListener('click', restartGame)
 overlay.addEventListener('click', closeMod)
 
-console.log(crossWon);
-console.log(naughtWon);
+// console.log(crossWon);
+// console.log(naughtWon);
+// console.log(count);
 
-
-// else if (i == answer.length) {
-//   result = "drawn game"
-//   prResult(result)
-// }
